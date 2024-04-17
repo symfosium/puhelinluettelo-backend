5cabc01abb0aca7882ce7ad8590a23ee8c4641df
Anton <symfosium@gmail.com>
2024-04-17 14:06:28 +0300

    bug fixes

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const cors = require('cors');
+const path = require('path');
 const app = express();
 
 app.use(express.json());
 app.use(cors());
-app.use(express.static('dist'));
+app.use(express.static(path.join(__dirname, 'dist')));
 
 let persons = [  
    {
@@ -23,6 +24,8 @@ let persons = [
       number: "09823-234"
    }
  ]
+
+ 
  app.get('/', (req, res) => {
    res.json('<h1>Minun Puhelinluettelo</h1>')
  })
